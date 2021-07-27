bb367de9487c028e107912fcab3d63d989468aa8
Madan <upretymadan45@gmail.com>
2021-07-27 13:25:02 +0545

    service auto start

--- LService/ProjectInstaller.cs
+++ LService/ProjectInstaller.cs
@@ -9,16 +9,40 @@ using System.Threading.Tasks;
 namespace LService
 {
     [RunInstaller(true)]
-    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    public partial class ProjectInstaller : Installer
     {
         public ProjectInstaller()
         {
-            InitializeComponent();
+            //InitializeComponent();
+            serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalService;
+            serviceProcessInstaller1.Password = null;
+            serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            serviceInstaller1.ServiceName = "LoginService";
+            serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+
+            serviceInstaller1.AfterInstall += new InstallEventHandler(this.serviceInstaller1_AfterInstall);
+            // 
+            // ProjectInstaller
+            // 
+            Installers.AddRange(new Installer[] {
+            serviceProcessInstaller1,
+            serviceInstaller1});
         }
 
         private void serviceInstaller1_AfterInstall(object sender, InstallEventArgs e)
         {
-
+            using (System.ServiceProcess.ServiceController serviceController = new System.ServiceProcess.ServiceController(serviceInstaller1.ServiceName))
+            {
+                serviceController.Start();
+            }
         }
     }
 }
